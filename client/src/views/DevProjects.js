df6c2e95d3f5cffc321950a9f7cfa8d4f956b478
Kamila <kamila.dynysiuk@gmail.com>
2020-10-06 15:42:31 +0100

    modified file/connect redux

--- client/src/views/DevProjects.js
+++ client/src/views/DevProjects.js
@@ -1,43 +1,9 @@
 import React from "react";
 import GridTemplate from "templates/GridTemplate";
+import { connect } from "react-redux";
 import Card from "components/molecules/Card/Card";
 
-const devProjects = [
-  {
-    id: 1,
-    title: "Vue.js ",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi",
-    created: "1 day",
-    name: "hello_roman",
-  },
-  {
-    id: 2,
-    title: "To do app",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi",
-    created: "1 day",
-    name: "My group",
-  },
-  {
-    id: 3,
-    title: "Forms to create",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi",
-    created: "5 days",
-    name: "Anna, Mike",
-  },
-  {
-    id: 4,
-    title: "Tech doucmentation",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi",
-    created: "10 days",
-    name: "Joanna",
-  },
-];
-
-const DevProjects = () => (
+const DevProjects = ({ devProjects }) => (
   <GridTemplate pageType="devprojects">
     {devProjects.map(({ title, content, name, created, id }) => (
       <Card
@@ -53,4 +19,12 @@ const DevProjects = () => (
   </GridTemplate>
 );
 
-export default DevProjects;
+DevProjects.defaulProps = {
+  devProjects: [],
+};
+const mapStateToProps = (state) => {
+  const { devProjects } = state;
+  return { devProjects };
+};
+
+export default connect(mapStateToProps)(DevProjects);
