739ecb71768e942308d43d27ba2d85f500947c46
Kamila <kamila.dynysiuk@gmail.com>
2020-10-08 14:43:08 +0100

    add close button style

--- client/src/components/atoms/Button/Button.js
+++ client/src/components/atoms/Button/Button.js
@@ -32,6 +32,38 @@ const Button = styled.button`
       height: 35px;
       font-size: 10px;
     `}
+
+    ${({ close }) =>
+      close &&
+      css`
+        background: ${({ activeColor, theme }) =>
+          activeColor ? theme[activeColor] : theme.notes};
+        width: 50px;
+        height: 50px;
+        position: relative;
+        margin-left: 88%;
+        top: -10px;
+
+        :after {
+          content: "";
+          height: 40px;
+          border-left: 2px solid #ccc;
+          position: absolute;
+          transform: rotate(45deg);
+          left: 25px;
+          top: 5px;
+        }
+
+        :before {
+          content: "";
+          height: 40px;
+          border-left: 2px solid #ccc;
+          position: absolute;
+          transform: rotate(-45deg);
+          left: 25px;
+          top: 5px;
+        }
+      `}
 `;
 
 export default Button;
